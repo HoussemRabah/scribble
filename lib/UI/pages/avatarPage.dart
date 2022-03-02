d558e14490351d17e266d4c9fec86278c3e02fd9
houssem <houssemranah@gmail.com>
2022-03-02 01:22:53 +0100

    add avatar UI

--- lib/UI/pages/avatarPage.dart
+++ lib/UI/pages/avatarPage.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:scribble/UI/pages/home.dart';
+
+import '../../constants.dart';
 
 class AvatarPage extends StatefulWidget {
   const AvatarPage({Key? key}) : super(key: key);
@@ -10,6 +14,40 @@ class AvatarPage extends StatefulWidget {
 class _AvatarPageState extends State<AvatarPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: colorBack,
+      body: Column(
+        children: [
+          Row(
+            children: [
+              SvgPicture.asset("assets/Boy.svg",
+                  width: 41, fit: BoxFit.fitWidth),
+              SvgPicture.asset("assets/The rock.svg",
+                  width: 59, fit: BoxFit.fitWidth),
+              SvgPicture.asset("assets/Girl.svg",
+                  width: 41, fit: BoxFit.fitWidth),
+            ],
+          ),
+          Text(userBloc.storage.username ?? "ops null error ):",
+              style: textStyleBig.copyWith(
+                color: Colors.white,
+              )),
+          //    Button(), X2
+          Row(
+            children: [
+              // ButtonSqaure()
+              // ButtonRound()
+              // ButtonSqaure()
+            ],
+          ),
+          Row(
+            children: [
+              //LeftArrow()
+              //RightArrow()
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/repository/storage_repo.dart
+++ lib/repository/storage_repo.dart
@@ -1,9 +1,12 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class StorageRepository {
+  String? username;
+
   Future<String?> getUser() async {
     SharedPreferences storageDb = await SharedPreferences.getInstance();
     await Future.delayed(Duration(milliseconds: 2000));
+    username = storageDb.getString("user");
 
     storageDb.remove("user");
     return storageDb.getString("user");
@@ -12,7 +15,7 @@ class StorageRepository {
   Future<bool> setUser(String user) async {
     SharedPreferences storageDb = await SharedPreferences.getInstance();
     await Future.delayed(Duration(milliseconds: 2000));
-
+    username = user;
     return storageDb.setString("user", user);
   }
 }
