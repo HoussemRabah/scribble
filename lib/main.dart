e788528984518750c958058ad5051cc22fed0b83
houssem <houssemranah@gmail.com>
2022-03-05 14:29:00 +0100

    add fullscrene

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'UI/pages/home.dart';
 import 'constants.dart';
@@ -7,6 +8,8 @@ import 'constants.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+
   runApp(const MyApp());
 }
 
